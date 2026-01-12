ec1f895def2237f6cc52f6a2f2ff38854aa9e672
Swaraj Ladke <swarajladke20@gmail.com>
2026-01-12 17:53:34 +0530

    fix: resolve CSS processing by properly configuring postcss.config.cjs for Next.js 13

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    outputFileTracingRoot: __dirname,
     experimental: {
         serverActions: true,
         serverActionsBodySizeLimit: "100mb",
